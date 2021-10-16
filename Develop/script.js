586596e1250a7bf6541efeb7954ccba2eee15368
lkalicki <lkalicki98@gmail.com>
2021-10-16 18:09:47 -0400

    add/enable random password generation and return to HTML

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,9 @@
-//THEN I am presented with a series of prompts for password criteria
-//WHEN prompted for password criteria
-//THEN I select which criteria to include in the password
-//WHEN prompted for the length of the password
-//THEN I choose a length of at least 8 characters and no more than 128 characters
-//WHEN asked for character types to include in the password
-//THEN I confirm whether or not to include lowercase, uppercase, numeric, and/or special characters
-//WHEN I answer each prompt
-//THEN my input should be validated and at least one character type should be selected
-//WHEN all prompts are answered
 //THEN a password is generated that matches the selected criteria
 //WHEN the password is generated
 //THEN the password is either displayed in an alert or written to the page
 
 function generatePassword() {
-  console.log("testing connection");
-  
+    
 //prompt for character length (8-128)
 var validateLength = parseInt(prompt("How many characters would you like your password to be? Please select a number between 8 and 128."))    
 
@@ -33,36 +22,49 @@ do {
   confirmSpecial = confirm("Would you like to include special characters?");
 } while (!confirmUpper && !confirmLower && !confirmNumbers && !confirmSpecial);
 
+// selected character types and length
+const length = validateLength;
+const Lower = confirmLower;
+const Upper = confirmUpper;
+const Numbers = confirmNumbers;
+const Special = confirmSpecial;
 
 
+//Generate Password
+ 
+const randomGeneration = {
+    Lower: RandomLower,
+    Upper: RandomUpper,
+    Numbers: RandomNumber,
+    Special: RandomSpecial
+  }
+  
+  //functions to retrieve random chars
+  function RandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+  }
+  function RandomUpper() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+  }
+  function RandomNumber() {
+    return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+  }
+  function RandomSpecial() {
+    return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
+  }
 
-//validate input 
 
-  return "generated password";
-}
+let password = '';
+const characterTypes = Lower + Upper + Numbers + Special;
+const characterArr = [{Lower}, {Upper}, {Numbers}, {Special}].filter(item => Object.values(item)[0]);
 
-const RandomGeneration = {
-  lower: RandomLower,
-  upper: RandomUpper,
-  number: RandomNumber,
-  symbol: RandomSpecial
-}
+for(let i=0; i<length; i+=characterTypes) {
+  characterArr.forEach(type => {
+    const charFunction = Object.keys(type)[0];
 
-//functions to retrieve random chars
-function RandomLower() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
-}
-
-function RandomUpper() {
-  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
-}
-
-function RandomNumber() {
-  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
-}
-
-function RandomSpecial() {
-  return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
+    password += randomGeneration[charFunction]();
+  });
+ } return password
 }
 
 // Get references to the #generate element
